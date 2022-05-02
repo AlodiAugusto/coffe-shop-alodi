824aab2daf3af6501f4e8b601f16f76b54a4a9ea
augusto <alodiaugusto@gmail.com>
2022-05-02 15:10:33 -0300

    actualización 1

---
+++ react/coffe-shop/src/components/NavBar.js
@@ -0,0 +1,13 @@
+function NavBar() {
+  return (
+    <div className="NavBar">
+        <h1>Ofertas Coffe Semana</h1>
+        <li className="lista">Oferta 1</li>
+        <li className="lista">Oferta 2</li>
+        <li className="lista">Oferta 3</li>
+        <li className="lista">Oferta 4</li>
+    </div>
+  );
+}
+
+export default NavBar;
